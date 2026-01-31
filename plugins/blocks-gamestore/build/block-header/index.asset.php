155c6abf79020947ad76bb8f79a0a5e71ef911f6
Oleksandr Voroniuk <voronyuk.aleksander@gmail.com>
2026-01-31 11:27:55 +0200

    Added header functionality

---
+++ plugins/blocks-gamestore/build/block-header/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '5bf533972b459f272796');
---
+++ plugins/blocks-gamestore/src/block-header/save.js
@@ -0,0 +1,25 @@
+
+import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
+
+export default function save({ attributes }) {
+	const { memberLink, cartLink } = attributes;
+	return (
+		<div { ...useBlockProps.save() }>
+			<div className='inner-header'>
+				<InnerBlocks.Content />
+				<div className='right-section'>
+					<div className='header-search'></div>
+					<div className='header-mode-switcher'></div>
+					{cartLink && (<div className='header-cart-link'>
+										<a href={cartLink}></a>
+									</div>)
+					}
+					{memberLink && (<div className='header-member'>
+										<a href={memberLink}>Member Area</a>
+									</div>)
+					}
+				</div>
+			</div>
+		</div>
+	);
+}
