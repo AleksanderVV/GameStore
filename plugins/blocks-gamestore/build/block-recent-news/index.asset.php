e2a68ff1dfa18e632dd96bb6cc9fd07de224ed04
Oleksandr Voroniuk <voronyuk.aleksander@gmail.com>
2026-02-12 16:21:58 +0200

    Added Recent news block

---
+++ plugins/blocks-gamestore/build/block-recent-news/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'eb397ba6778f4e5e998b');
---
+++ plugins/blocks-gamestore/build/block-recent-news/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'ed17d65b136366b073d2');
